792d3c69fbcc945f23aead97e62368e6eb138e90
Harshala Sawant <harshala.sawant@danaherdigital.com>
2022-01-27 11:00:33 +0530

    Initial styling changes for cover and login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NgxEchartsModule } from 'ngx-echarts';
 import { MatTableModule } from '@angular/material/table';
+import { LoginComponent } from './components/cover-page/login/login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoverPageComponent
+    CoverPageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatTableModule,
+    MatInputModule,
+    MatFormFieldModule,
     NgxEchartsModule.forRoot({
       echarts: () => import('echarts')
     })
